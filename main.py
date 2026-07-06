464436a58d03fa8e31043bdc8f27f8cb52d47dd1
faresavr1-dot <faresavr1@gmail.com>
2026-07-06 15:58:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ from pyrogram import Client, filters
 from pyrogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 # ================= إعدادات البوت =================
-API_ID = 1234567  # ضع الـ API_ID الخاص بك هنا
-API_HASH = "YOUR_API_HASH"  # ضع الـ API_HASH هنا
-BOT_TOKEN = "YOUR_BOT_TOKEN"  # ضع توكن البوت هنا
-ADMIN_ID = 123456789  # ضع آيدي المطور (الآيدي الخاص بك) هنا
+API_ID = 24217199 # ضع الـ API_ID الخاص بك هنا
+API_HASH = "11c12a66dbd23da592211771db1bce6b"  # ضع الـ API_HASH هنا
+BOT_TOKEN = "8849723725:AAE-bq0S4D7iPg3Cq9mQWVU99iFWkI_qxmg"  # ضع توكن البوت هنا
+ADMIN_ID = 7532687479  # ضع آيدي المطور (الآيدي الخاص بك) هنا
 
 app = Client("HostingManager", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
